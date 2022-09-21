c1730797379431d6eca9a9b391cb5fd5f4aa8587
Isadora Riegert <isadora.riegert@gmail.com>
2022-09-21 11:25:47 +0100

    feat(task): create delete task button

--- src/pages/ListScreen.tsx
+++ src/pages/ListScreen.tsx
@@ -1,59 +1,70 @@
-import { nanoid } from "nanoid";
-import React, { ChangeEvent, KeyboardEvent, useState } from "react"
-import { TaskTS } from "../models/Task";
+import { nanoid } from 'nanoid';
+import React, { ChangeEvent, KeyboardEvent, useState } from 'react';
+import { TaskTS } from '../models/Task';
 
 function ListScreen() {
-    const [tasks, setTasks] = useState<TaskTS[]>([]);
-    const [newTaskLabel, setNewTaskLabel] = useState('');
+  const [tasks, setTasks] = useState<TaskTS[]>([]);
+  const [newTaskLabel, setNewTaskLabel] = useState('');
 
-    const handleNewTaskLabelChange = (e: ChangeEvent<HTMLInputElement>) => setNewTaskLabel(e.target.value);
-    const handleNewTaskKeypress = (e: KeyboardEvent<HTMLInputElement>) => {
-        if (e.key === 'Enter' && (e.currentTarget.value.trim().length != 0)) { //validation to deny insert blank spaces
-            setTasks((tasks) => [...tasks, {id: nanoid(), label: newTaskLabel, isComplete: false}])
-            setNewTaskLabel(''); //to clear the input
-        }
-    };
-
-    const handleCompleteChange = 
-    (handleTask: TaskTS) => (e: ChangeEvent<HTMLInputElement>) => {
-        setTasks((tasks) =>
-            tasks.map((task) => {
-                if (task.id === handleTask.id) 
-                    return {...task, isComplete: e.target.checked};
-                return task;
-        }));
-    };
-
-    const handleClearClick = () => {
-        setTasks(tasks => tasks.filter(task => !task.isComplete))
+  const handleNewTaskLabelChange = (e: ChangeEvent<HTMLInputElement>) =>
+    setNewTaskLabel(e.target.value);
+  const handleNewTaskKeypress = (e: KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === 'Enter' && e.currentTarget.value.trim().length != 0) {
+      //validation to deny insert blank spaces
+      setTasks((tasks) => [
+        ...tasks,
+        { id: nanoid(), label: newTaskLabel, isComplete: false },
+      ]);
+      setNewTaskLabel(''); //to clear the input
     }
+  };
 
-    console.log(tasks)
+  const handleTaskCompleteChange =
+    (handleTask: TaskTS) => (e: ChangeEvent<HTMLInputElement>) => {
+      setTasks((tasks) =>
+        tasks.map((task) => {
+          if (task.id === handleTask.id)
+            return { ...task, isComplete: e.target.checked };
+          return task;
+        })
+      );
+    };
 
-    return (
+  const handleTaskDeleteClick = (handledTask: TaskTS) => () => {
+    setTasks((tasks) => tasks.filter((task) => task.id !== handledTask.id));
+  };
+
+  const handleClearClick = () => {
+    setTasks((tasks) => tasks.filter((task) => !task.isComplete));
+  };
+
+  console.log(tasks);
+
+  return (
     <div>
-        <div>
-            {tasks.map((task) => (
-                <div key={task.id}>
-                    <input 
-                    type="checkbox" 
-                    checked={task.isComplete} 
-                    onChange={handleCompleteChange(task)}
-                    />
-                    {task.label}
-                </div>
-            ))}
-        </div>
-        <input 
-        value={newTaskLabel} 
+      <div>
+        {tasks.map((task) => (
+          <div key={task.id}>
+            <input
+              type="checkbox"
+              checked={task.isComplete}
+              onChange={handleTaskCompleteChange(task)}
+            />
+            {task.label}
+            <button onClick={handleTaskDeleteClick(task)}>delete</button>
+          </div>
+        ))}
+      </div>
+      <input
+        value={newTaskLabel}
         onChange={handleNewTaskLabelChange}
         onKeyPress={handleNewTaskKeypress}
-        />
-        <div>
-            <button onClick={handleClearClick}>Clear Completed Tasks</button>
-        </div>
+      />
+      <div>
+        <button onClick={handleClearClick}>Clear Completed Tasks</button>
+      </div>
     </div>
-    )
+  );
 }
 
 export default ListScreen;
